1107fc2fee9a66b75baa5e1b1952cd790f725e7c
Justin Falcone <stanulam59@gmail.com>
2013-05-13 14:08:39 -0400

    basic outline?

---
+++ js/script-min.js
@@ -0,0 +1,2 @@
+/* global Popcorn:true*/// wrap console.log
+if(window.console===undefined){window.console={};window.console.log=function(){}}$(document).ready(function(){Popcorn("#the-video")});
--- js/script.js
+++ js/script.js
@@ -1,5 +1,15 @@
-// console.log wrapper
+/* global Popcorn:true*/
+
+// wrap console.log
 if (window.console === undefined) {
   window.console = {};
   window.console.log = function() {};
 }
+
+$(document).ready(function() {
+  Popcorn('#the-video');
+
+
+
+
+});
